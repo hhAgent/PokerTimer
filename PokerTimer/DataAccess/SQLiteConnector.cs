a5223dde41e3d2ecf0eefcdbaeebd1c2de3f403a
Hieu Minh Nguyen <Hieu Minh Nguyen@HHAGENT>
2016-12-14 01:35:12 +0700

    Add database file, SqliteConnector

--- PokerTimer/DataAccess/SQLiteConnector.cs
+++ PokerTimer/DataAccess/SQLiteConnector.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,42 @@ namespace PokerTimer.DataAccess
 {
     public class SQLiteConnector
     {
+        protected static SQLiteConnection m_dbConnection;
+        public static void Init()
+        {
+            string databasePath = Directory.GetCurrentDirectory() + "\\poker.db";
+            m_dbConnection = new SQLiteConnection(
+                string.Format("Data Source={0};Version=3;", databasePath)
+                );
+            m_dbConnection.Open();
+        }
 
+        public static string TestConnection()
+        {
+            try
+            {
+                string sql = "SELECT * FROM sqlite_master WHERE type='table';";
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                SQLiteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    if (reader["name"] != null)
+                    {
+                        reader.Close();
+                        return string.Empty;
+                    }
+                }
+                return "DB Empty";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message + " " + Environment.NewLine + ex.StackTrace;
+            }
+        }
+
+        public static void Close()
+        {
+            m_dbConnection.Close();
+        }
     }
 }
