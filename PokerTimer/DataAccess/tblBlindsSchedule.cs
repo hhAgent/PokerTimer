c669be659c80614f9535b79a50ee6c66de11f587
Hieu Minh Nguyen <Hieu Minh Nguyen@HHAGENT>
2016-12-14 16:36:53 +0700

    SQLite Tables Data Access

--- PokerTimer/DataAccess/tblBlindsSchedule.cs
+++ PokerTimer/DataAccess/tblBlindsSchedule.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PokerTimer.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,50 @@ namespace PokerTimer.DataAccess
 {
     public class tblBlindsSchedule : SQLiteConnector
     {
+        public static List<BlindsLevel> GetBlindsLevelByTournamentId(long tourId)
+        {
+            try
+            {
+                string sql = string.Format("SELECT * FROM blindschedule WHERE tournamentid={0} ORDER BY stage", tourId );
+                
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                SQLiteDataReader reader = command.ExecuteReader();
 
+                List<BlindsLevel> res = new List<BlindsLevel>();
+                while (reader.Read())
+                {
+                    res.Add(new BlindsLevel()
+                    {                        
+                        TournamentId = long.Parse( reader["id"].ToString() ),
+                        Stage = int.Parse( reader["stage"].ToString() ),
+                        Level = int.Parse( reader["level"].ToString() ),
+                        Length = decimal.Parse( reader["length"].ToString() ),
+                        SmallBlind = int.Parse( reader["smallblind"].ToString() ),
+                        BigBlind = int.Parse( reader["bigblind"].ToString() ),
+                        Ante = int.Parse( reader["ante"].ToString() ),
+                    });
+                }
+                return res;
+            }
+            catch
+            {
+                return new List<BlindsLevel>();
+            }
+        }
+
+        public static int DeleteTournamentId(long tourId)
+        {
+            try
+            {
+                string sql = string.Format("DELETE FROM blindschedule WHERE tournamentid={0}", tourId);
+
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                return command.ExecuteNonQuery();                
+            }
+            catch
+            {
+                return 0;
+            }
+        }
     }
 }
--- PokerTimer/DataAccess/tblTournament.cs
+++ PokerTimer/DataAccess/tblTournament.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using PokerTimer.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +10,68 @@ namespace PokerTimer.DataAccess
 {
     public class tblTournament : SQLiteConnector
     {
+        public static List<Tournament> GetAllTournament()
+        {
+            return GetTournamentById(-1);
+        }
+
+        public static List<Tournament> GetTournamentById(long id = -1)
+        {
+            try
+            {
+                string sql = string.Empty;
+                if (id < 0)
+                {
+                    sql = "SELECT * FROM tournament";
+                }
+                else
+                {
+                    sql = "SELECT * FROM tournament where id=" + id;
+                }
+
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                SQLiteDataReader reader = command.ExecuteReader();
+
+                List<Tournament> res = new List<Tournament>();
+                while (reader.Read())
+                {
+                    res.Add(new Tournament()
+                    {
+                        Name = reader["name"].ToString(),
+                        Id = long.Parse(reader["id"].ToString()),
+                        StartingTime = DateTime.ParseExact(reader["starttime"].ToString(), "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture),
+                        Rebuy = int.Parse(reader["rebuy"].ToString()),
+                        Addon = int.Parse(reader["addon"].ToString()),
+                        UpdateTime = DateTime.ParseExact(reader["updatetime"].ToString(), "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture),
+                        TotalPlayers = int.Parse( reader["totalplayers"].ToString() ),
+                        CurrentPlayers = int.Parse(reader["currentplayers"].ToString() ),
+                        PrizePool = decimal.Parse( reader["prizepool"].ToString() ),
+                        AvgStack = decimal.Parse( reader["avgstack"].ToString() ),
+                        LastStage = int.Parse( reader["laststage"].ToString() ),
+                        LastStageTime = int.Parse( reader["laststagetime"].ToString() )
+                    });
+                }
+                return res;
+            }
+            catch
+            {
+                return new List<Tournament>();
+            }
+        }
+
+        public static int DeleteTournamentById(long tourId)
+        {
+            try
+            {
+                string sql = string.Format("DELETE FROM tournament WHERE id={0}", tourId);
+
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                return command.ExecuteNonQuery();
+            }
+            catch
+            {
+                return 0;
+            }
+        }
     }
 }
