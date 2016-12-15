4ede89c3d6a226c37be3760dd85492eefd234fe9
Hieu Minh Nguyen <Hieu Minh Nguyen@HHAGENT>
2016-12-15 16:56:36 +0700

    how model's fields are displayed

--- PokerTimer/Models/Tournament.cs
+++ PokerTimer/Models/Tournament.cs
@@ -7,32 +7,75 @@ namespace PokerTimer.Models
 {
     public class Tournament
     {
+        /// <summary>
+        /// Database tự sinh, không cần quản lý
+        /// </summary>
         public long Id { get; set; }
 
+        /// <summary>
+        /// Hiển thị trong các trang Add / Edit General tournament
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Hiển thị trong các trang Add / Edit General tournament
+        /// </summary>
         public int Rebuy { get; set; }
 
+        /// <summary>
+        /// Hiển thị trong các trang Add / Edit General tournament
+        /// </summary>
         public int StartingChips { get; set; }
 
+        /// <summary>
+        /// Hiển thị trong các trang Add / Edit General tournament
+        /// </summary>
         public int Addon { get; set; }
 
-        public int TotalPlayers { get; set; }
-
-        public int CurrentPlayers { get; set; }
-
-        public decimal PrizePool { get; set; }
-
-        public decimal AvgStack { get; set; }
-
-        public DateTime UpdateTime { get; set; }
-
+        /// <summary>
+        /// Hiển thị trong các trang Add / Edit Clock tournament
+        /// </summary>
         public DateTime StartingTime { get; set; }
 
+        /// <summary>
+        /// Hiển thị trong trang Edit Clock tournament
+        /// </summary>
         public int LastStageTime { get; set; }
 
+        /// <summary>
+        /// Hiển thị trong trang Edit Clock tournament
+        /// </summary>
         public int LastStage { get; set; }
 
+        /// <summary>
+        /// Cứ mỗi lần tương tác add/edit gì tournament thì gán nó là = DateTime.Now
+        /// </summary>
+        public DateTime UpdateTime { get; set; }
+
+        /// <summary>
+        /// Hiển thị trong trang Edit Clock Tournament
+        /// </summary>
         public bool IsStopped { get; set; }
+
+        /// <summary>
+        /// Hiển thị trong trang Edit General Tournament
+        /// </summary>
+        public int TotalPlayers { get; set; }
+
+        /// <summary>
+        /// Hiển thị trong trang Edit General Tournament
+        /// </summary>
+        public int CurrentPlayers { get; set; }
+
+        /// <summary>
+        /// Hiển thị trong trang Edit General Tournament
+        /// </summary>
+        public decimal PrizePool { get; set; }
+
+        /// <summary>
+        /// Hiển thị trong trang Edit General Tournament
+        /// </summary>
+        public decimal AvgStack { get; set; }
+
     }
 }
